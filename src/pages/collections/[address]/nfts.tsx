9c1463c914178ec0307ca243c6a35d9a0db861cb
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-08-17 14:17:58 +0200

    fix: big screen grid increase

--- src/pages/collections/[address]/nfts.tsx
+++ src/pages/collections/[address]/nfts.tsx
@@ -134,7 +134,7 @@ export default function CollectionNfts() {
             'grid grid-cols-1 gap-4 pt-4',
             open
               ? 'md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
-              : 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'
+              : 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8'
           )}
         >
           {nftsQuery.loading ? (
--- src/pages/profiles/[address]/collected.tsx
+++ src/pages/profiles/[address]/collected.tsx
@@ -136,7 +136,7 @@ export default function ProfileCollected() {
             'grid grid-cols-1 gap-4 pt-4',
             open
               ? 'md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
-              : 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'
+              : 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8'
           )}
         >
           {nftsQuery.loading ? (
