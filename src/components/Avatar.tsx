d83040df755fd10cac2f3afd264cd78c6616fe83
Milo Rue <milorue@gmail.com>
2022-08-25 08:35:59 -0700

    wip: update graphql to codegen

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,91 @@
+import { useLazyQuery } from '@apollo/client';
+import { PublicKey } from '@solana/web3.js';
+import clsx from 'clsx';
+import { useEffect } from 'react';
+import { Wallet } from '../graphql.types';
+import { getProfileInfoQuery } from '../queries/getProfileInfoFromPubkey.graphql';
+
+enum AvatarSize {
+  Small = 'sm',
+  Medium = 'md',
+  Large = 'lg',
+}
+
+interface ProfileData {
+  wallet: Wallet;
+}
+
+interface ProfileVariables {
+  pubKey: string;
+}
+
+interface AvatarProps {
+  image?: string;
+  address: string;
+  size?: AvatarSize;
+  enableProfile?: boolean; // enables fetching twitter handle/image
+}
+
+function Avatar({ image, size = AvatarSize.Medium, enableProfile = true, address }: AvatarProps) {
+  const [profileQuery, profileContext] = useLazyQuery<ProfileData, ProfileVariables>(
+    getProfileInfoQuery
+  );
+
+  useEffect(() => {
+    async function queryProfileData(address: string) {
+      if (enableProfile) {
+        profileQuery({
+          variables: {
+            pubKey: address,
+          },
+        });
+      }
+    }
+    if (!image) queryProfileData(address);
+  }, [profileQuery, image, address, enableProfile]);
+
+  if (image) {
+    return (
+      <img
+        src={image}
+        alt={address}
+        className={clsx(clsx, 'rounded-full', {
+          'h-10 w-10': size === AvatarSize.Medium,
+          'h-6 w-6': size === AvatarSize.Small,
+          'h-12 w-12': size === AvatarSize.Large,
+        })}
+      />
+    );
+  }
+
+  return (
+    <>
+      {enableProfile &&
+      profileContext.called &&
+      !profileContext.loading &&
+      profileContext?.data?.wallet.profile ? (
+        <img
+          src={profileContext.data?.wallet.profile?.profileImageUrlLowres}
+          alt={address}
+          className={clsx(clsx, 'rounded-full', {
+            'h-10 w-10': size === AvatarSize.Medium,
+            'h-6 w-6': size === AvatarSize.Small,
+            'h-12 w-12': size === AvatarSize.Large,
+          })}
+        />
+      ) : (
+        <img
+          src={'/images/placeholder.png'}
+          alt={address}
+          className={clsx(clsx, 'rounded-full', {
+            'h-10 w-10': size === AvatarSize.Medium,
+            'h-6 w-6': size === AvatarSize.Small,
+            'h-12 w-12': size === AvatarSize.Large,
+          })}
+        />
+      )}
+    </>
+  );
+}
+
+export default Avatar;
---
+++ src/pages/nfts/[address]/offers.tsx
@@ -0,0 +1,102 @@
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { Marketplace, Nft, Offer } from '../../../graphql.types';
+import client from './../../../client';
+import NftQuery from './../../../queries/nft.graphql';
+import NftOffersQuery from './../../../queries/offers.graphql';
+import { GetServerSidePropsContext } from 'next';
+import config from '../../../app.config';
+import { useWallet } from '@solana/wallet-adapter-react';
+import NftLayout from '../../../layouts/NftLayout';
+import { ReactNode } from 'react';
+import { useQuery } from '@apollo/client';
+import Avatar from '../../../components/Avatar';
+
+export async function getServerSideProps({ locale, params }: GetServerSidePropsContext) {
+  const i18n = await serverSideTranslations(locale as string, ['common', 'offers', 'nft']);
+
+  const {
+    data: { nft, marketplace },
+  } = await client.query({
+    query: NftQuery,
+    variables: {
+      address: params?.address,
+      subdomain: config.marketplaceSubdomain,
+    },
+  });
+
+  if (nft === null) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      nft,
+      marketplace,
+      ...i18n,
+    },
+  };
+}
+
+interface NFTOffersVariables {
+  address: string;
+}
+
+interface NFTOffersData {
+  nftOffers: Partial<Nft>;
+}
+
+interface NftOfferPageProps {
+  nft: Nft;
+  marketplace: Marketplace;
+}
+
+export default function NftOffers({ nft, marketplace }: NftOfferPageProps) {
+  marketplace;
+  const { t } = useTranslation('offers');
+
+  const { publicKey } = useWallet();
+
+  const isOwner = nft.owner.address === publicKey?.toBase58();
+
+  const { data, refetch } = useQuery<NFTOffersData, NFTOffersVariables>(NftOffersQuery, {
+    variables: {
+      address: nft.mintAddress,
+    },
+  });
+
+  return (
+    <>
+      {data?.nftOffers?.offers?.length === 0 && (
+        <div className="flex w-full justify-center rounded-lg border border-gray-800 p-4">
+          <h3 className="text-gray-300">{t('noOffers')}</h3>
+        </div>
+      )}
+      <div className="flex flex-col gap-4">
+        {data?.nftOffers?.offers?.map((offer, i) => (
+          <div
+            key={`offer-${offer.id}-${i}`}
+            className="flex gap-2 rounded-lg border border-gray-800 p-4"
+          >
+            <Avatar address={offer.buyer} />
+            <div></div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+interface NftDetailsLayoutProps {
+  children: ReactNode;
+  nft: Nft;
+}
+
+NftOffers.getLayout = function NftDetailsLayout({
+  children,
+  nft,
+}: NftDetailsLayoutProps): JSX.Element {
+  return <NftLayout nft={nft}>{children}</NftLayout>;
+};
