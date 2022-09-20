8b4045c90d0203bdfc0b3fabc6a4908f5ae5f973
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-09-20 15:41:31 +0200

    feat: footer + app config

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,6 +3,11 @@ interface AppConfig {
   graphqlUrl: string;
   solanaRPCUrl: string;
   marketplaceSubdomain: string;
+  socialMedia: {
+    twitter?: string;
+    discord?: string;
+    medium?: string;
+  };
 }
 
 const config: AppConfig = {
@@ -10,6 +15,11 @@ const config: AppConfig = {
   graphqlUrl: process.env.NEXT_PUBLIC_GRAPHQL_URL as string,
   solanaRPCUrl: process.env.NEXT_PUBLIC_SOLANA_RPC_URL as string,
   marketplaceSubdomain: process.env.NEXT_PUBLIC_MARKETPLACE_SUBDOMAIN as string,
+  socialMedia: {
+    twitter: '#',
+    discord: '#',
+    medium: '#',
+  },
 };
 
 export default config;
