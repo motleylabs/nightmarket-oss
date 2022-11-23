b81ef6d384bacc27e8dd5ebd47842c4ba1c853bb
Anshul Goel <anshul.go87@gmail.com>
2022-11-23 20:21:23 +0530

    feat: add filter labels to collection page

--- src/pages/collections/[id]/nfts.tsx
+++ src/pages/collections/[id]/nfts.tsx
@@ -18,13 +18,20 @@ import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 import { NftCard } from '../../../components/NftCard';
 import { List, ListGridSize } from '../../../components/List';
-import { Listbox, Switch } from '@headlessui/react';
+import { Listbox } from '@headlessui/react';
 import { Attribute } from '../../../components/Attribute';
 import { Offerable } from '../../../components/Offerable';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { Buyable } from '../../../components/Buyable';
 import Select from '../../../components/Select';
 import config from '../../../app.config';
+import Button, {
+  ButtonBackground,
+  ButtonBorder,
+  ButtonColor,
+  ButtonSize,
+} from '../../../components/Button';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 export async function getServerSideProps({ locale, params }: GetServerSidePropsContext) {
   const i18n = await serverSideTranslations(locale as string, ['common', 'collection']);
@@ -72,12 +79,6 @@ interface CollectionAttributeGroupsVariables {
   id: string;
 }
 
-enum ListedStatus {
-  All = 'all',
-  Listed = 'listed',
-  Unlisted = 'unlisted',
-}
-
 interface CollectionNFTForm {
   attributes: { [key: string]: string[] };
   sortBySelect: SortType;
@@ -114,9 +115,35 @@ export default function CollectionNfts() {
     { value: SortType.RecentlyListed, label: t('sort.recentlyListed') },
   ];
 
-  const { watch, control } = useForm<CollectionNFTForm>({
+  const { watch, control, resetField, setValue } = useForm<CollectionNFTForm>({
     defaultValues: { sortBySelect: sortOptions[0].value },
   });
+
+  const attributes = watch('attributes');
+
+  const selectedAttributes: string[] = [];
+  if (attributes) {
+    Object.entries(attributes).forEach(
+      ([key, value]) =>
+        value && value.forEach((attribute) => selectedAttributes.push(`${key}: ${attribute}`))
+    );
+  }
+
+  const onClearClick = () => {
+    resetField('attributes');
+  };
+
+  const onRemoveFilter = (filterLabel: string) => {
+    const [group, attribute] = filterLabel.split(': ', 2);
+    const newAttributes: {
+      [key: string]: string[];
+    } = {};
+    Object.entries(attributes).forEach(
+      ([key, value]) =>
+        (newAttributes[key] = value ? value.filter((v) => v !== attribute && key === group) : [])
+    );
+    setValue('attributes', newAttributes);
+  };
 
   const attributeGroupsQuery = useQuery<
     CollectionAttributeGroupsData,
@@ -189,54 +216,90 @@ export default function CollectionNfts() {
       </Toolbar>
       <Sidebar.Page open={open}>
         <Sidebar.Panel onChange={toggleSidebar}>
-          <div className="mt-4 flex w-full flex-col gap-2">
-            {attributeGroupsQuery.loading ? (
-              <>
-                <Attribute.Skeleton />
-                <Attribute.Skeleton />
-                <Attribute.Skeleton />
-                <Attribute.Skeleton />
-                <Attribute.Skeleton />
-              </>
-            ) : (
-              <>
-                {attributeGroupsQuery.data?.collection?.attributeGroups.map((group, index) => (
-                  <div key={group.name} className=" w-full rounded-2xl bg-gray-800 p-4">
-                    <Controller
-                      key={group.name}
-                      control={control}
-                      name={`attributes.${group.name}`}
-                      render={({ field: { onChange, value } }) => (
-                        <Listbox
-                          multiple
-                          value={value || []}
-                          onChange={(e) => {
-                            onChange(e);
-                          }}
-                        >
-                          {({ open }) => (
-                            <>
-                              <Listbox.Button className=" flex w-full items-center justify-between">
-                                <Attribute.Header group={group} isOpen={open} />
-                              </Listbox.Button>
-                              <Listbox.Options className={'mt-6 space-y-4'}>
-                                {group.variants.map((variant) => (
-                                  <Listbox.Option key={variant.name} value={variant.name}>
-                                    {({ selected }) => (
-                                      <Attribute.Option variant={variant} selected={selected} />
-                                    )}
-                                  </Listbox.Option>
-                                ))}
-                              </Listbox.Options>
-                            </>
-                          )}
-                        </Listbox>
-                      )}
-                    />
-                  </div>
-                ))}
-              </>
+          <div className="mt-6 flex w-full flex-col gap-6">
+            {selectedAttributes.length > 0 && (
+              <div className="flex flex-col gap-2 md:hidden">
+                <span className="text-sm text-gray-200">Filters:</span>
+                <div className="flex flex-wrap gap-2">
+                  <>
+                    {selectedAttributes.map((attribute) => (
+                      <div
+                        className="rounded-full bg-primary-900 bg-opacity-10 py-2 px-4 text-sm text-primary-500"
+                        key={attribute}
+                      >
+                        <div className="flex gap-2">
+                          {attribute}{' '}
+                          <XMarkIcon
+                            className="h-4 w-4 cursor-pointer"
+                            onClick={() => onRemoveFilter(attribute)}
+                          />
+                        </div>
+                      </div>
+                    ))}
+                    {selectedAttributes.length > 1 && (
+                      <Button
+                        background={ButtonBackground.Black}
+                        border={ButtonBorder.Gradient}
+                        color={ButtonColor.Gradient}
+                        size={ButtonSize.Tiny}
+                        onClick={onClearClick}
+                      >
+                        {t('common:clear')}
+                      </Button>
+                    )}
+                  </>
+                </div>
+              </div>
             )}
+            <div className="flex flex-col gap-2">
+              {attributeGroupsQuery.loading ? (
+                <>
+                  <Attribute.Skeleton />
+                  <Attribute.Skeleton />
+                  <Attribute.Skeleton />
+                  <Attribute.Skeleton />
+                  <Attribute.Skeleton />
+                </>
+              ) : (
+                <>
+                  {attributeGroupsQuery.data?.collection?.attributeGroups.map((group, index) => (
+                    <div key={group.name} className=" w-full rounded-2xl bg-gray-800 p-4">
+                      <Controller
+                        key={group.name}
+                        control={control}
+                        name={`attributes.${group.name}`}
+                        render={({ field: { onChange, value } }) => (
+                          <Listbox
+                            multiple
+                            value={value || []}
+                            onChange={(e) => {
+                              onChange(e);
+                            }}
+                          >
+                            {({ open }) => (
+                              <>
+                                <Listbox.Button className=" flex w-full items-center justify-between">
+                                  <Attribute.Header group={group} isOpen={open} />
+                                </Listbox.Button>
+                                <Listbox.Options className={'mt-6 space-y-4'}>
+                                  {group.variants.map((variant) => (
+                                    <Listbox.Option key={variant.name} value={variant.name}>
+                                      {({ selected }) => (
+                                        <Attribute.Option variant={variant} selected={selected} />
+                                      )}
+                                    </Listbox.Option>
+                                  ))}
+                                </Listbox.Options>
+                              </>
+                            )}
+                          </Listbox>
+                        )}
+                      />
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
           </div>
         </Sidebar.Panel>
         <Sidebar.Content>
