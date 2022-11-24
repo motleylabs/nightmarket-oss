83b0cf573798e2555cfb05af623b285943ab0f65
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-11-24 20:11:20 +0100

    fix: renamed variable

--- src/client.ts
+++ src/client.ts
@@ -643,6 +643,7 @@ const client = new ApolloClient({
             read(dateString: string) {
               // this could potentially be moved to the backend
               // it needs to be in unix for ReCharts to process it more efficently
+
               return new Date(dateString).getTime();
             },
           },
@@ -680,7 +681,7 @@ const client = new ApolloClient({
               return datapoints || [];
             },
           },
-          holderCount: {
+          ownersCount: {
             read(holderCount: Datapoint[]): Datapoint[] {
               const datapoints = holderCount?.map((point: Datapoint) => ({
                 ...point,
