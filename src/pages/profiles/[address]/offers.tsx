90984e2d396642f1905a2041fa75055b74a191c1
WaggMe <callmewagg@gmail.com>
2023-02-01 11:51:10 -0700

    fix deprecated explicit-ns syntax for translations

--- src/pages/profiles/[address]/offers.tsx
+++ src/pages/profiles/[address]/offers.tsx
@@ -84,9 +84,9 @@ export default function ProfileOffers({ auctionHouse }: ProfileOfferPageProps): 
   const { publicKey } = useWallet();
 
   const activityFilterOptions = [
-    { label: t('profile:offersFilter.allOffers'), value: OffersFilter.All },
-    { label: t('profile:offersFilter.offersReceived'), value: OffersFilter.Received },
-    { label: t('profile:offersFilter.offersPlaced'), value: OffersFilter.Placed },
+    { label: t('offersFilter.allOffers', { ns: 'profile' }), value: OffersFilter.All },
+    { label: t('offersFilter.offersReceived', { ns: 'profile' }), value: OffersFilter.Received },
+    { label: t('offersFilter.offersPlaced', { ns: 'profile' }), value: OffersFilter.Placed },
   ];
 
   const { watch, control } = useForm<ProfileOffersForm>({
