102b4ee608a8f55afdb56ebfca3096a454931274
omar <omar.kilani@gmail.com>
2023-05-01 11:57:34 -0700

    Buy buffer fixes.

--- src/utils/hyperspace.ts
+++ src/utils/hyperspace.ts
@@ -1,20 +1,27 @@
 import axios from 'axios';
+import { Base64 } from 'js-base64';
 
 export const getBuyNowTransaction = async (
+  auctionHouseProgram: string,
+  auctionHouseAddress: string,
+  seller: string,
   buyer: string,
   price: string,
   tokenAddress: string
 ): Promise<Buffer | null> => {
   try {
     const { data } = await axios.post(`${process.env.NEXT_PUBLIC_ANDROMEDA_ENDPOINT}/nfts/buy`, {
+      auctionHouseProgram: auctionHouseProgram,
+      auctionHouseAddress: auctionHouseAddress,
+      seller: seller,
       buyer,
       buyerBroker: buyer,
       price,
       mint: tokenAddress,
     });
 
-    if (data.data.length > 0) {
-      return Buffer.from(data.data);
+    if (data.buffer.length > 0) {
+      return Buffer.from(Base64.toUint8Array(data.buffer));
     }
   } catch (e: unknown) {
     // eslint-disable-next-line no-console
--- src/utils/marketplaces.ts
+++ src/utils/marketplaces.ts
@@ -6,7 +6,7 @@ export interface Marketplace {
   name: string;
   link: string;
   logo: string;
-  buyNowEnabled: boolean;
+  buyNowEnabled?: boolean;
 }
 
 const multiTenantMarkets: Map<string, boolean> = new Map([
